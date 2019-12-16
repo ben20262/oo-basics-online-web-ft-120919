13613c0a5787084a688ac59884fb2657174652c9
Benjamin Small <ben20262@aim.com>
2019-12-16 15:58:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,8 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def cobble
+    puts "This shoe has been repaired."
+    @condition = "new"
+  end
+end
